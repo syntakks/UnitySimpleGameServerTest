bfa7192c3451d8a9b8c21d7538871b4228959e9e
syntakks <syntakks@gmail.com>
2020-02-01 00:31:11 -0600

    SocketIO package added
    Network client added.
    Observable server objects added to empty game object parent.

---
+++ Assets/Scripts/Networking/NetworkClient.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO; 
+
+namespace Project.Networking
+{
+    public class NetworkClient : SocketIOComponent
+    {
+        [Header("Network Client")]
+        [SerializeField] private Transform networkContainer;
+
+        private Dictionary<string, GameObject> serverObjects; 
+
+        public override void Start()
+        {
+            base.Start();
+            Initialize(); 
+            SetupEvents(); 
+        }
+
+        public override void Update()
+        {
+            base.Update(); 
+        }
+
+        private void Initialize()
+        {
+            serverObjects = new Dictionary<string, GameObject>(); 
+        }
+
+        private void SetupEvents()
+        {
+            On("open", (E) => {
+                Debug.Log("Connection made to the server!"); 
+            });
+
+            On("register", (E) => {
+                string id = E.data["id"].ToString().Trim('"'); 
+                Debug.LogFormat("Register: ClientID: ({0})", id); 
+            });
+
+            On("spawn", (E) => {
+                // Handling spawning all players
+                // Passed data
+                string id = E.data["id"].ToString().Trim('"');
+
+                GameObject go = new GameObject("Server ID: " + id);
+                go.transform.SetParent(networkContainer); // Keep it organized. 
+                serverObjects.Add(id, go); // Add to current roster. 
+            });
+
+            On("disconnected", (E) => {
+                string id = E.data["id"].ToString().Trim('"');
+                GameObject go = serverObjects[id];
+                Destroy(go);
+                serverObjects.Remove(id); 
+            });
+        }
+    }
+}
+
